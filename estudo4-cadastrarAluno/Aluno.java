62af3b59f670b6c6ba7db55380cdd4431c5fbed0
Renan Augusto <114965499+RenanAUG@users.noreply.github.com>
2023-08-29 20:53:59 -0300

    Projeto - Cadastrar Aluno

---
+++ estudo4-cadastrarAluno/Aluno.java
@@ -0,0 +1,28 @@
+package com.example.exemploactivity;
+
+public class Aluno {
+
+    private int ra;
+    private String nome;
+
+    public Aluno(int ra, String nome) {
+        this.ra = ra;
+        this.nome = nome;
+    }
+
+    public int getRa() {
+        return ra;
+    }
+
+    public void setRa(int ra) {
+        this.ra = ra;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
